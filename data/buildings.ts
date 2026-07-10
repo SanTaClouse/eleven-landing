740fcc838ecc7a38b26b6a460143d1a7358eb13b
Santiago <santiagoriv06@gmail.com>
2026-07-10 12:23:14 -0300

    add edificios pro

--- data/buildings.ts
+++ data/buildings.ts
@@ -10,24 +10,16 @@
 export const MAINTAINED_BUILDINGS_COUNT = 60;
 
 export const BUILDING_SHOWCASE: string[] = [
+  "CAM 100",
   "Torre Gaudí",
   "Portal Sur",
-  "Neo 1825",
   "Oliver",
   "Coveiq XIII",
-  "Torre 1 El Pozo",
-  "Plaza España",
-  "Clínica Wagner",
   "Portales III",
   "Mediterráneo III",
   "Olivia",
   "Celestino XIV",
   "Pilar del Teatro",
   "Salón Monumental VIP",
-  "SJ50",
   "Sembrando I",
-  "9 de Julio 3418",
-  "Crespo 3133",
-  "Junín 2681",
-  "Aristóbulo del Valle 5181",
 ];
